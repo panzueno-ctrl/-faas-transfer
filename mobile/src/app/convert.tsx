af3d7178019b133245374b015ca9e7ad411d62d4
panzueno <panzueno@gmail.com>
2026-09-26 15:26:03 +0200

    Fix: Import loadPdfJs for thumbnail rendering

--- mobile/src/app/convert.tsx
+++ mobile/src/app/convert.tsx
@@ -42,7 +42,7 @@ import PdfEditor, { PdfEditItem } from '../components/PdfEditor';
 import CompressEditor from '../components/CompressEditor';
 import { PDFDocument, rgb, StandardFonts, degrees } from 'pdf-lib/dist/pdf-lib.esm.js';
 import JSZip from 'jszip';
-import { usePdfEngine } from '../features/convert/hooks/usePdfEngine';
+import { usePdfEngine, loadPdfJs } from '../features/convert/hooks/usePdfEngine';
 import { useConvertApi } from '../features/convert/hooks/useConvertApi';
 import ResultScreen from '../features/convert/ui/ResultScreen';
 import ProcessingScreen from '../features/convert/ui/ProcessingScreen';
